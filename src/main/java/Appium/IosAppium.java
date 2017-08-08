6cf9fc244c5ffe0112f785c21567fd4c27f6668e
何汪兵 <hewangbing@aiyoumi.com>
2017-08-08 21:04:11 +0800

    1. 更新pom文件 2. 更新IosAppium文件

--- src/main/java/Appium/IosAppium.java
+++ src/main/java/Appium/IosAppium.java
@@ -1,8 +1,8 @@
 package Appium;
 
-
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.pagefactory.iOSFindBy;
+import io.appium.java_client.pagefactory.iOSXCUITBy;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -14,22 +14,30 @@ public class IosAppium {
     private static IOSDriver<WebElement> driver;
 
     public static void main(String[] args) throws MalformedURLException {
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("automationName", "XCUITest");
         capabilities.setCapability("platformVersion", "10.3");
         capabilities.setCapability("deviceName", "iPhone 6s");
         capabilities.setCapability("platformName", "iOS");
         capabilities.setCapability("udid", "07fe7964897592db864978df79c0bf003ea94f7f");
-        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3600);
+//        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 3600);
         capabilities.setCapability("bundleId", "com.aixuedai.axd");
-        capabilities.setCapability("noReset", true);
-        driver = new IOSDriver<>(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("useNewWDA", "false");
+        driver = new IOSDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         System.out.println("启动成功");
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("结束进程");
+        driver.findElementByIosNsPredicate("type='XCUIElementTypeStaticText' AND label='我的'").click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("点击我的，然后结束进程");
     }
 }
